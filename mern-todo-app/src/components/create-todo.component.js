fcf38124578e8eab720af60076432f09c22e2d4f
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-10-17 22:08:05 +0000

    Part 4

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default  class CreateTodo extends Component {
     constructor(props) {
@@ -38,13 +39,24 @@ export default  class CreateTodo extends Component {
     onSubmit (e) {
         e.preventDefault();
 
-        //submit logic insert here
+        //submit logic insert here using axios as http POST request
         console.log(`Form submitted:`);
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
         console.log(`Todo Completed: ${this.state.todo_completed}`);
 
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        }
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+            .then(res => console.log(res.data));
+
+        //reset the fields to be blank
         this.setState({
             todo_description: '',
             todo_responsible: '',
