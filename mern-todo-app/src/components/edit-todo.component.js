57c104b73cdbfea69d45b87cadb83d7b1c9ae834
Charlie Fu <charlie.zhe.fu@gmail.com>
2019-10-18 00:27:05 +0000

    Part 4 completed

--- mern-todo-app/src/components/edit-todo.component.js
+++ mern-todo-app/src/components/edit-todo.component.js
@@ -31,7 +31,72 @@ export default  class EditTodo extends Component {
     render () {
         return (
             <div>
-                <p>Welcome to Edit Todo Component!</p>
+                <h3>Update Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.todo_description}
+                            onChange={this.onChangeTodoDescription}
+                            />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text"
+                               className="form-control"
+                               value={this.state.todo_responsible}
+                               onChange={this.onChangeTodoResponsible}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityLow"
+                                value="Low"
+                                checked={this.state.todo_priority==='Low'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityMedium"
+                                value="Medium"
+                                checked={this.state.todo_priority==='Medium'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityHigh"
+                                value="High"
+                                checked={this.state.todo_priority==='High'}
+                                onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-check">   
+                        <input  type="checkbox"  
+                                className="btn btn-primary" 
+                                id="completedCheckBox"
+                                onChange={this.onChangeTodoCompleted}
+                                checked={this.state.todo_completed}
+                                value={this.state.todo_completed}
+                                />
+                        <label className="form-check-label" htmlFor="completedCheckBox">
+                            Completed
+                        </label>
+                    </div>
+                </form>
             </div>
         )
     }
